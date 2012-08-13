5427036a2ac60b5e67a0d0e6dd337e82e2a7f7a7
Paul Winkler <slinkp@gmail.com>
2012-08-13 15:04:08 -0400

    * Support dumping issues to disk for use with unofficial bulk import process * Related refactoring * Support copying trac resolutions to github labels * Remove -m option now that Github has milestones * Better usage message * Convert 'defect' label to 'bug', for github default label compatibility * Support users being specified via email address - (only works with bulk   import, not API)

--- trac2issues.py
+++ trac2issues.py
@@ -11,22 +11,27 @@ from time import sleep
 ##Setup pp for debugging
 pp = pprint.PrettyPrinter(indent=4)
 
+usage = """Usage: %prog [options] action
+action may be one of 'import', 'dump'
+"""
+parser = OptionParser(usage=usage)
 
-parser = OptionParser()
 parser.add_option('-t', '--trac', dest='trac', help='Path to the Trac project to export.')
 parser.add_option('-a', '--account', dest='account', help='Name of the GitHub Account to import into. (If not specified, user from your global git config will be used.)')
 parser.add_option('-p', '--project', dest='project', help='Name of the GitHub Project to import into.')
 parser.add_option('-x', '--closed', action="store_true", default=False, dest='closed', help='Include closed tickets.')
 parser.add_option('-y', '--type', action="store_true", default=False, dest='type', help='Create a label for the Trac ticket type.')
 parser.add_option('-c', '--component', action="store_true", default=False, dest='component', help='Create a label for the Trac component.')
-parser.add_option('-m', '--milestone', action="store_true", default=False, dest='milestone', help='Create a label for the Trac milestone.')
+# parser.add_option('-m', '--milestone', action="store_true", default=False, dest='milestone', help='Create a label for the Trac milestone.')
 parser.add_option('-r', '--reporter', action="store_true", default=False, dest='reporter', help='Create a label for the Trac reporter.')
-parser.add_option('-o', '--owner', action="store_true", default=False, dest='owner', help='Create a label for the Trac owner.')
+parser.add_option('-o', '--owner', action="store_true", default=False, dest='owner', help='Create a label for the Trac owner/assignee.')
+parser.add_option('-n', '--resolution', action="store_true", default=False,
+                  help="Create a label for the Trac ticket resolution.")
 parser.add_option('-u', '--url', dest='url', help='Base URL for the Trac install (if specified, will create a link to the old ticket in a comment).')
 parser.add_option('-g', '--org', dest='organization', help='Name of GitHub Organization (supercedes --account)')
 parser.add_option('-s', '--start', dest='start', help='The trac ticket to start importing at.')
 parser.add_option('--authors', dest='authors_file', default='authors.txt',
-                  help='File to load assignee login names from. Each line is space-separated like: trac-login github-login')
+                  help='File to load user login names from. Each line is space-separated like: trac-login github-login')
 (options, args) = parser.parse_args(sys.argv[1:])
 
 
@@ -72,7 +77,7 @@ def urlopen(*args, **kw):
 
 class ImportTickets:
 
-    def __init__(self, trac=options.trac, account=options.account, project=options.project):
+    def __init__(self, trac=options.trac, account=options.account, project=options.project, authors_file=options.authors_file):
         self.env = open_environment(trac)
         self.trac = trac
         self.account = account
@@ -88,22 +93,30 @@ class ImportTickets:
 
         self.includeClosed = options.closed
         self.labelType = options.type
-        self.labelMilestone = options.milestone
+        self.authors_file = authors_file
         self.labelComponent = options.component
+        self.labelResolution = options.resolution
         self.labelOwner = options.owner
         self.labelReporter = options.reporter
         self.start = options.start
         self.useURL = False
         self.organization = options.organization
         self.reqCount = 0
-
+        self.milestones = {}  # Mapping of title -> id.
+        self.contributors = {}
+        self._milestones_created = set()
         if options.url:
-            self.useURL = "%s/ticket/" % options.url
+            self.useURL = "%s/ticket/" % (options.url.rstrip('/'))
 
-        self.ghAuth()
-
+        self.login = self.password = None
         self.projectPath = '%s/%s' % (self.organization or self.account or self.login, self.project)
 
+        self._typemap = {
+            'defect': 'bug',
+            }
+
+    def importAllToGithub(self):
+        self.ghAuth()
         self.checkProject()
         self.milestones = self.loadMilestones()
         self.contributors = self.loadContributors()
@@ -116,9 +129,9 @@ class ImportTickets:
             if go[0:1] != 'y':
                 print_error('Try Again..')
 
-
         ##We own this project..
-        self._fetchTickets()
+        self.importAllTickets()
+
 
     def checkProject(self):
         url = "%s/repos/%s" % (self.github, self.projectPath)
@@ -152,11 +165,11 @@ class ImportTickets:
             else:
                 where = ' where id >= %s' % self.start
 
-        sql = "select id, summary, status, description, milestone, component, reporter, owner, type from ticket %s order by id" % where
+        sql = "select id, summary, status, description, milestone, component, reporter, owner, type, resolution from ticket %s order by id" % where
         cursor.execute(sql)
         # iterate through resultset
         tickets = []
-        for id, summary, status, description, milestone, component, reporter, owner, type in cursor:
+        for id, summary, status, description, milestone, component, reporter, owner, type, resolution in cursor:
             if milestone:
                 milestone = milestone.replace(' ', '_')
             if component:
@@ -180,6 +193,7 @@ class ImportTickets:
                 'history': [],
                 'status': status,
                 'type': type,
+                'resolution': resolution,
             }
             cursor2 = self.db.cursor()
             sql = 'select author, time, newvalue from ticket_change where (ticket = %s) and (field = "comment")' % id
@@ -194,23 +208,14 @@ class ImportTickets:
 
             tickets.append(ticket)
 
-        print bold('About to import (%s) tickets from Trac to %s.\n%s? [y/N]' % (len(tickets), self.projectPath, red('Are you sure you wish to continue')))
-        go = sys.stdin.readline().strip().lower()
+        return tickets
 
-        if go[0:1] != 'y':
-            print_error('Import Aborted..')
-
-
-        #pp.pprint(tickets)
-        for data in tickets:
-            self.createIssue(data)
-
-
-    def createIssue(self, info):
-        print bold('Creating issue from ticket %s' % info['id'])
+    def prepareIssue(self, info):
+        """Make a github-compatible dictionary representing the issue.
+        """
         out = {
             'title': info['summary'].encode('utf-8'),
-            'body': info['description'].encode('utf-8'),
+            'body': markdown_from_trac(info['description']).encode('utf-8'),
             'labels': [],
         }
 
@@ -220,30 +225,101 @@ class ImportTickets:
                 return value
             return False
 
-        if self.labelMilestone and info_has_key('milestone'):
-            out['milestone'] = self.getMilestone(info['milestone'])
+        if info_has_key('milestone'):
+            title = info['milestone']
+            if title not in self.milestones:
+                # We assume this number isn't there. TODO: make robust.
+                m_id = len(self.milestones) + 1
+                assert m_id not in self.milestones.values()
+                self.milestones[title] = m_id
+            out['milestone'] = self.milestones[title]
+
 
         if self.labelType and info_has_key('type'):
-            out['labels'].append(info['type'])
+            _type = self._typemap.get(info['type'], info['type'])
+            out['labels'].append(_type)
 
         if self.labelComponent and info_has_key('component'):
             out['labels'].append(info['component'])
 
-        if self.labelOwner and info_has_key('owner'):
-            if info['owner'] in self.contributors:
-                out['assignee'] = self.contributors[info['owner']]
+        if self.labelResolution and info_has_key('status') and info_has_key('resolution'):
+            if info['status'] == 'closed':
+                if info['resolution'] != 'fixed':  # too boring to include.
+                    out['labels'].append(info['resolution'])
 
-        if self.labelReporter and info_has_key('reporter'):
+        if info_has_key('owner'):
+            owner = info['owner']
+            if owner in self.contributors:
+                owner = self.contributors[owner]
+            out['assignee'] = owner
+            if self.labelOwner:
+                out['labels'].append('@@%s' % owner)
+
+        if info_has_key('reporter'):
             # Unfortunately github api v3 still has no way to specify the
-            # creator of an issue.
-            out['labels'].append("@@%s" % info['reporter'])
+            # creator of an issue. Via the API we can work around with a label...
+            if self.labelReporter:
+                out['labels'].append("@@%s" % info['reporter'])
+            # Otherwise, if using the unofficial bulk dump process, we can set it.
+            out['creator'] = self.parse_user(info['reporter'])
 
+        comments = []
+        for i in info['history']:
+            if i['comment']:
+                body = i['comment'].strip().encode('utf-8', 'replace')
+                # Ignore tracback comments for now.
+                if 'class="tracback"' in body:
+                    continue
+                comment = {'body': markdown_from_trac(body)}
+                author = i.get('author', 'anonymous').strip()
+                comment['user'] = self.parse_user(author)
+                comments.append(comment)
+
+        if self.useURL:
+            comment = "Ticket imported from Trac:\n %s%s" % (self.useURL, info['id'])
+            comment += "\nReported by: %s" % info['reporter']
+            comments.append({'body': comment})
+
+        out['labels'] = list(set([l.encode('utf-8', 'ignore') for l in out['labels']]))
+        # TODO created/closed/modified timestamps.
+        if info['status'] == 'closed':
+            out['state'] = 'closed'
+
+        return out, comments
+
+
+    def parse_user(self, author):
+        """Returns a dict with user and/or email keys.
+        """
+        if '@' in author:
+            email = author.split('<', 1)[-1].split('>', 1)[0]
+            return {'email': email}
+        else:
+            return {'login': author.encode('utf-8', 'replace')}
+
+    def createIssueViaAPI(self, info):
+        """Add an issue via github API."""
+        print bold('Creating issue from ticket %s' % info['id'])
+        out, comments = self.prepareIssue(info)
+
+
+        # Remove bulk-import format stuff that the API can't deal with
+        #ticket.pop('creator')
         for label in out['labels']:
             # Labels must exist before being assigned to tickets.
             self.createLabel(label)
 
+        if out.get('milestone'):
+            # Likewise milestones.
+            self.getOrCreateMilestone(out['milestone'])
+
         url = "%s/repos/%s/issues" % (self.github, self.projectPath)
-        response = self.makeRequest(url, out)
+        try:
+            response = self.makeRequest(url, out)
+        except:
+            #import pdb; pdb.set_trace()
+            return
+
         ticket_data = simplejson.load(response)
 
         if 'number' in ticket_data:
@@ -252,23 +328,14 @@ class ImportTickets:
         else:
             print_error('GitHub didn\'t return an issue number :(')
 
-        for i in info['history']:
-            if i['comment']: 
-                if i['author']:
-                    comment = "Author: %s\n%s" % (i['author'].encode('utf-8','replace'), i['comment'].encode('utf-8','replace'))
-                else:
-                    comment = i['comment'].encode('utf-8','replace')
-                    
-                self.addComment(num, comment)
-
-        if self.useURL:
-            comment = "Ticket imported from Trac:\n %s%s" % (self.useURL, info['id'])
+        for comment in comments:
             self.addComment(num, comment)
 
         if info.get('status') == 'closed':
             self.closeTicket(num)
 
     def createLabel(self, name):
+        """Create a label via the API, if it doesn't already exist."""
         # Can't add a label to a ticket unless it exists, humph.
         if name in self.labels:
             return
@@ -280,9 +347,25 @@ class ImportTickets:
         self.makeRequest(url, out)
         self.labels.add(name)
 
-    def getMilestone(self, name):
-        if name in self.milestones:
-            return self.milestones[name]
+    def getOrCreateMilestone(self, name_or_number):
+        # TODO: handle state, description, due date.
+        try:
+            num = int(name_or_number)
+            # Reverse lookup, assuming each value is unique
+            # zip(*foo() makes list of keys and list of vals, in matching order.
+            allnames, allnums = zip(*self.milestones.items())
+            if num in allnums:
+                name = allnames[allnums.index(num)]
+            else:
+                name = None
+        except (TypeError, ValueError):
+            name = name_or_number
+            num = self.milestones.get(name)
+
+        if num in self._milestones_created:
+            return
+
+        # New milestone, create it.
         url = "%s/repos/%s/milestones" % (self.github, self.projectPath)
         out = {
             'title': name
@@ -291,12 +374,15 @@ class ImportTickets:
         milestone_data = simplejson.load(response)
         num = milestone_data['number']
         self.milestones[name] = num
+        self._milestones_created.add(num)
         return num
 
     def loadMilestones(self):
         milestones = {}
         self.loadMilestonesForStatus('open', milestones)
         self.loadMilestonesForStatus('closed', milestones)
+        for m in milestones.values():
+            self._milestones_created.add(m)
         return milestones
 
     def loadMilestonesForStatus(self, param, milestones):
@@ -328,16 +414,12 @@ class ImportTickets:
         return set(labels)
 
     def addComment(self, num, comment):
-        comment = comment.strip()
         if not comment:
             print bold("\tSkipping empty comment on issue # %s" % num)
             return
         print bold("\tAdding comment to issue # %s" % num)
         url = "%s/repos/%s/issues/%s/comments" % (self.github, self.projectPath, num)
-        out = {
-            'body': comment
-        }
-        response = self.makeRequest(url, out)
+        response = self.makeRequest(url, comment)
 
     def closeTicket(self, num):
         url = "%s/repos/%s/issues/%s" % (self.github, self.projectPath, num)
@@ -353,14 +435,17 @@ class ImportTickets:
                         '%s:%s' % (self.login, self.password))[:-1]
         authheader =  "Basic %s" % base64string
         req.add_header("Authorization", authheader)
+        # Setting content type explicitly to avoid known bug where github
+        # occasionally barfs on content that includes a '%'.
+        req.add_header('Content-Type', 'application/json')
         print url
-        print json.dumps(out)
+        #print json.dumps(out)
         self.reqCount += 1
         if (self.reqCount % GITHUB_MAX_PER_MINUTE == 0):
             self.apiLimitExceeded()
         print "Request no: %s" % (self.reqCount)
         try:
-            response = urllib2.urlopen(req)
+            response = urlopen(req)
         except urllib2.HTTPError, err:
             if err.code == 403:
                self.apiLimitExceeded()
@@ -375,9 +460,67 @@ class ImportTickets:
         return response
 
     def apiLimitExceeded(self):
-        self.reqCount = 0
-        print "Sleeping for 60 seconds"
-        sleep(60)
+        # Don't need this if we always use our own urlopen()
+        pass
+        # self.reqCount = 0
+        # print "Sleeping for 60 seconds"
+        # sleep(60)
+
+
+    def importAllTickets(self):
+        tickets = self._fetchTickets()
+        print bold('About to import (%s) tickets from Trac to %s.\n%s? [y/N]' % (len(tickets), self.projectPath, red('Are you sure you wish to continue')))
+        go = sys.stdin.readline().strip().lower()
+
+        if go[0:1] != 'y':
+            print_error('Import Aborted..')
+        #pp.pprint(tickets)
+        for data in tickets:
+            self.createIssueViaAPI(data)
+
+
+    def dumpAllIssues(self, issuedir):
+        """
+        Useful with the bulk-import-issues beta https://gist.github.com/7f75ced1fa7576412901
+        """
+        tickets = self._fetchTickets()
+        for ticket in tickets:
+            i = ticket['id']
+            ticket, comments = self.prepareIssue(ticket)
+            ticket_filename = os.path.join(issuedir, '%s.json' % i)
+            with file(ticket_filename, 'w') as outfile:
+                json.dump(ticket, outfile, indent=1)
+            comments_filename = os.path.join(outdir, 'issues', '%s.comments.json' % i)
+            with file(comments_filename, 'w') as outfile:
+                json.dump(comments, outfile, indent=1)
+
+    def dumpAllMilestones(self, milestonedir):
+        # TODO: handle state, description, due date.
+        for name, number in self.milestones.items():
+            milestone_filename = os.path.join(milestonedir, '%s.json' % number)
+            with file(milestone_filename, 'w') as outfile:
+                json.dump({'title': name}, outfile, indent=1)
+
+    def dumpAll(self, outdir):
+        issuedir = os.path.join(outdir, 'issues')
+        if not os.path.isdir(issuedir):
+            os.makedirs(issuedir)
+        self.dumpAllIssues(issuedir)
+
+        # Have to do milestones second since milestones are discovered
+        # while iterating over issues.
+        milestonedir = os.path.join(outdir, 'milestones')
+        if not os.path.isdir(milestonedir):
+            os.makedirs(milestonedir)
+        self.dumpAllMilestones(milestonedir)
+
+
+def markdown_from_trac(text):
+    # Quick hack to convert some notable trac wiki formatting stuff
+    # to equivalent markdown syntax.
+    text = text.replace('{{{', '```')
+    text = text.replace('}}}', '```')
+    return text
 
 def urlencode_utf8(adict):
     """Ensure dict's values are all utf-8 before urlencoding it.
@@ -400,17 +543,27 @@ def print_error(str):
     sys.exit(1)
 
 
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print "For usage: %s --help" % (sys.argv[0])
-        print
-    else:
-        if not options.trac or not options.project:
-            print_error("For usage: %s --help" % (sys.argv[0]))
 
-        os.environ['PYTHON_EGG_CACHE'] = '/tmp/.egg-cache'
-        os.environ['TRAC_ENV'] = options.trac
-        from trac.core import TracError
-        from trac.env import open_environment
-        from trac.util.datefmt import utc
-        ImportTickets()
+if __name__ == "__main__":
+    if not (args and options.trac and options.project):
+        print_error("You need at least an action, and  the -t and -p options. For usage: %s --help" % (sys.argv[0]))
+
+    os.environ['PYTHON_EGG_CACHE'] = '/tmp/.egg-cache'
+    os.environ['TRAC_ENV'] = options.trac
+    from trac.core import TracError
+    from trac.env import open_environment
+    from trac.util.datefmt import utc
+
+    importer = ImportTickets()
+    if args[0] == 'import':
+        importer.importAllToGithub()
+    elif args[0] == 'dump':
+        try:
+            outdir = args[1]
+        except IndexError:
+            print_error("Dump action needs an output directory specified")
+            sys.exit(1)
+        importer.dumpAll(outdir)
+        print "Your output is in %s" % outdir
+    else:
+        print_error("Need to specify a valid action, either dump or import")
